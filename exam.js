214360428681a3da62e654d42d90ea83994d4f2a
hoang <hoang160102@gmail.com>
2021-03-02 20:29:56 +0700

    commit test

---
+++ exam.js
@@ -0,0 +1,28 @@
+//ex2//
+//a
+//for (let i=1; i<=9; i++){
+//console.log(i);
+//}
+//b
+//for (let i=14; i<100; i+=7){
+    //console.log(i);
+//}
+//c
+//let a = Number(prompt("enter a"));
+//let b = Number(prompt("enter b"));
+//let n = Number(prompt("enter n"));
+//for (let i=a; i<=b; i++){
+   //if (i%n==0){
+    //console.log(i);
+  /// }
+//}
+//d
+//let m = Number(prompt("enter m"));
+//let n = Number(prompt("enter n"));
+//for (i=m; i<=n; i++){
+
+//}
+
+//e 
+//let m = prompt(Number("enter m"));
+
