f766c26179275f483d2b9ba767bc68b5b5aa226c
hoang <hoang160102@gmail.com>
2021-03-02 18:18:31 +0700

    first commit

---
+++ lesson1.js
@@ -0,0 +1,69 @@
+/*Ex1*/
+/*Ex1a*/
+/* you will get the SyntaxError telling you that some
+of your variables have invalid names when */
+// Declaring twice triggers an error //
+// Reserved names //
+// An assignment without use strict //
+/*3 different examples of invalid variable name*/
+/*1*/
+// let massage = "This"; //
+// let massage = "That"; //
+//SyntaxError: 'message' has already been declared  //
+/*2*/
+// let let 5; //
+// SyntaxError: Unexpected strict mode reserved word //
+/*3*/ 
+// let 3;//
+// SyntaxError: Unexpected number //
+/*Ex1b*/
+// use typeof() to check variable data types //
+
+/*Ex2*/
+// 1-b; 2-a; 3-e; 4c //
+
+/*Ex3*/
+/*Ex3a*/
+// let message ="Coding is great"; //
+// console.log(message); //
+/*Ex3b*/
+// let studentCount =0; //
+// console.log(studentCount); //
+
+/*Ex4*/
+/*Ex4a*/
+// let message ="Coding might not be easy, but still great"; //
+// console.log(message);//
+/*Ex4b*/
+// let studentCount =16;//
+// console.log(studentCount);//
+/*Ex4c*/
+// let message = "Coding might not be easy, but still great";//
+// console.log(message.toLowerCase()); //
+/*Ex4d*/
+//let studentCount =16;//
+//studentCount++;//
+//console.log(studentCount);//
+
+/*Ex5*/
+//alert ("You look beautiful today");//
+
+/*Ex6*/
+//let message =prompt("Hi there, your name please?");//
+//alert (`Hi ${name}`);//
+
+/*Ex7*/
+//let firstname =prompt("Enter your first name?");//
+//let lastname =prompt("Enter your last name?");//
+//alert (`Hi ${first name} ${last name}');//
+
+/*Ex8*/
+//let length =prompt("Enter side length of the square");//
+//alert("The square area is : " + Math.pow(length,2));//
+ 
+/*Ex9*/
+//let radius =prompt("Enter the radius of the circle");//
+//alert("The circle area is : " + Math.pow(radius,2)*3.14);//
+
+/*Ex10*/
+//let celsius = prompt ("Enter the temperature in Celsius");//
---
+++ lesson2.js
@@ -0,0 +1,96 @@
+//let name1 = prompt("Nhap vao ten mon");
+//let cost1 = prompt("Nhap vao gia tuong ung");
+//let name2 = prompt("Nhap vao ten mon");
+//let cost2 = prompt("Nhap vao gia tuong ung");
+//let name3 = prompt("Nhap vao ten mon");
+//let cost3 = prompt("Nhap vao gia tuong ung");
+
+//console.log("MENU");
+//console.log(`${name1} ${cost1}`);
+//console.log(`${name2} ${cost2}`);
+//console.log(`${name3} ${cost3}`);
+
+// for (let i = 0; i<10; i++) {
+// let name = prompt("Nhap vao ten mon: ");
+// let cost = prompt("Nhap vao gia tuong ung: ");
+// console.log(`${name} ${cost}`);
+// }
+// in day so tu 0 den 100
+//for (let i = 0; i<=100; i++) {
+  //  console.log(i);
+//}
+// day so tu 100 den 1
+//for (let i=100; i>=0; i--) {
+    //console.log(i);
+//}
+ 
+// day so chan tu 0 den 50
+//for (let i=0; i<=50; i+=2) {
+  //  console.log(i);
+//}
+
+// in ra da so tu m den n (m, n do nguoi dung nhap)
+//INPUT: m va n (Cach 1)
+//let m = Number(prompt("enter: "));
+//let n = Number(prompt("enter: "));
+//OUTOUT: day so tu m den n
+//for (let i = m; i<=n; i++) {
+//console.log(i);
+//}
+/*cach 2*/
+//let m = Number(prompt("enter m: "));
+//let n = Number(prompt("enter n:"));
+//while (m<=n) {
+    //console.log(m);
+   // m++
+//}
+
+// in ra day so tu 3 den n, cac so chia het cho 3
+//let n = Number(prompt("enter n: "));
+//for (let i = 3; i<=n; i+=3) {
+  //  console.log(i);
+//}
+
+ //in ra day so tu m den n, cac so chia het cho 3
+//let m = Number(prompt("enter m: "));
+//let n = Number(prompt("enter n: "));
+//for (let i = m; i<=n; i++) {
+   // if (i%3 == 0) {
+  //      console.log("chia het cho 3");
+   // } else if (i%5 == 0) {
+   //     console.log("chia het cho 5");
+  //  } else {
+  //      console.log("failed");
+  //  }
+//}
+
+// yêu cầu người dùng nhập tuổi, từ đó in ra xem người dùng đã đủ tuổi lấy vợ chưa
+// nếu tuổi <10 => trẻ con
+// nếu tuổi <20 => chưa đủ tuổi
+// nếu tuổi <60 => oke
+// nếu tuổi >60 => già quá
+//let m = Number(prompt("Nhap so tuoi cua ban"));
+//if (m<10) {
+    //console.log("tre con");
+// 
+//}else if (m<20) {
+  //  console.log("chua di tuoi");
+//} else if (m<60) {
+    //console.log("oke");
+// else {
+ //   console.log("gia qua");
+//}
+
+/* cau truc if :
+if (statement) {
+    //block
+} else if (statement) {
+    // block
+} else {
+} */
+
+//while (true) {
+   // let age = Number(prompt("Enter your age: "));
+//} if (age == 0) {
+    //break;
+//}
