e924f4455950d42d577d5096f36bc6f42ac47684
hoang160102 <77884607+hoang160102@users.noreply.github.com>
2021-03-16 20:53:20 +0700

    Add files via upload

---
+++ checkpoint/checkpoint.js
@@ -0,0 +1,11 @@
+//ex1
+//console.log(Math.random())
+//ex2
+//use code: let randomValue = myArray[Math.floor(Math.random() * myArray.length)];
+//example
+// let myNumbers = [3,4,5,6,7,7,8]
+// let numbers = myNumbers[Math.floor(Math.random()) * myNumbers.length]
+// console.log(myNumbers)
+// console.log(numbers)
+//ex3
+
